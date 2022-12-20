8c02b7d1f2b4e60c3f42a088caf9173cc1c802a0
kickinbrain <kickinbrain@gmail.com>
2022-12-20 19:48:24 +0400

    completed episode 7 - Tennis Match Kata

--- src/TennisMatch.php
+++ src/TennisMatch.php
@@ -9,23 +9,100 @@ class TennisMatch
 
     public function score()
     {
-        if($this->playerOnePoints > $this->playerTwoPoints + 2){
-            return 'forty-love';
-        }
-        if($this->playerOnePoints > $this->playerTwoPoints + 1){
-            return 'thirty-love';
+        if($this->hasWinner()){
+            return "Winner: " . $this->leader();
         }
 
-        if($this->playerOnePoints > $this->playerTwoPoints){
-            return 'fifteen-love';
+        if($this->hasAdvantage()){
+            return "Advantage: " . $this->leader();
         }
 
-        return 'love-love';
+        if($this->isDeuce()){
+            return "deuce";
+        }
+
+       return sprintf(
+           "%s-%s",
+           $this->pointsToTerm($this->playerOnePoints),
+           $this->pointsToTerm($this->playerTwoPoints)
+       );
     }
 
     public function pointToPLayerOne()
     {
         $this->playerOnePoints++;
     }
+
+    public function pointToPLayerTwo()
+    {
+        $this->playerTwoPoints++;
+    }
+
+
+    protected function pointsToTerm($points){
+        switch ($points){
+            case 0:
+                return "love";
+            case 1:
+                return "fifteen";
+            case 2:
+                return "thirty";
+            case 3:
+                return "forty";
+
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasWinner(): bool
+    {
+        if($this->playerOnePoints > 3 && $this->playerOnePoints >= $this->playerTwoPoints + 2){
+            return true;
+        }
+        if($this->playerTwoPoints > 3 && $this->playerTwoPoints >= $this->playerOnePoints + 2){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function leader(): string
+    {
+        return $this->playerOnePoints > $this->playerTwoPoints
+            ? "Player 1"
+            : "Player 2";
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDeuce(): bool
+    {
+        $canBewWon = $this->canBeWon();
+
+        return $canBewWon && $this->playerOnePoints === $this->playerTwoPoints;
+    }
+
+    protected function hasAdvantage()
+    {
+        if($this->canBeWon()){
+            return ! $this->isDeuce();
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canBeWon(): bool
+    {
+        return $this->playerOnePoints >= 3 && $this->playerTwoPoints >= 3;
+    }
+
 
 }
--- tests/TennisMatchTest.php
+++ tests/TennisMatchTest.php
@@ -15,6 +15,9 @@ class TennisMatchTest extends TestCase
         for($i = 0; $i < $playerOnePoints; $i++){
             $match->pointToPLayerOne();
         }
+        for($i = 0; $i < $playerTwoPoints; $i++){
+            $match->pointToPLayerTwo();
+        }
 
         $this->assertEquals($score, $match->score());
     }
@@ -24,8 +27,16 @@ class TennisMatchTest extends TestCase
         return [
           [0, 0, 'love-love'],
           [1, 0, 'fifteen-love'],
+          [1, 1, 'fifteen-fifteen'],
           [2, 0, 'thirty-love'],
-          [3, 0, 'forty-love']
+          [2,2, 'thirty-thirty'],
+          [3, 0, 'forty-love'],
+          [10,10, 'deuce'],
+          [4,3,'Advantage: Player 1'],
+          [3,4,'Advantage: Player 2'],
+          [4, 0, 'Winner: Player 1'],
+          [0, 4, 'Winner: Player 2'],
+          [3,3, 'deuce']
         ];
     }
 }
