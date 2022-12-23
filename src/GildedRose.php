63111bdd24ee7bfb77774ed67e8237455038e885
kickinbrain <kickinbrain@gmail.com>
2022-12-23 14:25:24 +0400

    completed half of episode 9 - GildedRose Kata

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,6 +24,24 @@ class GildedRose
 
     public function tick()
     {
+
+        if($this->name === 'normal'){
+            return $this->normalTick();
+        }
+        
+        if($this->name === "Aged Brie"){
+            return $this->brieTick();
+        }
+        
+        if($this->name === 'Sulfuras, Hand of Ragnaros'){
+            return $this->sulfTick();
+        }
+
+        
+        if($this->name === 'Backstage passes to a TAFKAL80ETC concert'){
+            return $this->backstagePossesTick();
+        }
+
         if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
             if ($this->quality > 0) {
                 if ($this->name != 'Sulfuras, Hand of Ragnaros') {
@@ -69,6 +87,55 @@ class GildedRose
                     $this->quality = $this->quality + 1;
                 }
             }
+        }
+    }
+
+    private function normalTick()
+    {
+        $this->sellIn  -= 1;
+        $this->quality -= 1;
+
+        if($this->sellIn <= 0){
+            $this->quality -= 1;
+        }
+
+        if($this->quality < 0){
+            $this->quality = 0;
+        }
+    }
+    private function brieTick()
+    {
+        $this->sellIn -= 1;
+        $this->quality += 1;
+
+        if($this->sellIn <= 0){
+            $this->quality +=1 ;
+        }
+
+        if($this->quality > 50){
+            $this->quality = 50;
+        }
+    }
+
+    private function sulfTick()
+    {
+
+    }
+
+    private function backstagePossesTick()
+    {
+        $this->sellIn -= 1;
+        $this->quality += 1;
+
+        if($this->sellIn <= 10){
+            $this->quality += 1;
+        }
+        if($this->sellIn <= 5){
+            $this->quality += 1;
+        }
+
+        if($this->sellIn <= 0){
+            $this->quality = 0;
         }
     }
 }
